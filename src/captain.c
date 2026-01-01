5cc6d60fcb5d8828907024bfda13b4185300819e
Monika Toczek <moniat598@gmail.com>
2026-01-01 19:58:12 +0100

    Dodanie szkieletow procesow: kapitan, dyspozytor, pasazer

---
+++ src/captain.c
@@ -0,0 +1,6 @@
+#include "common.h"
+
+int main(int argc, char *argv[]) {
+    printf("Captain process started\n");
+    return 0;
+} 
---
+++ src/dispatcher.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+int main(int argc, char *argv[]) {
+   printf("Dispatcher process started\n");
+   return 0;
+}
+
