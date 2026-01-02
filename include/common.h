ed4a301ecd6c8396c0ab5928668a23eb57c68813
Monika Toczek <moniat598@gmail.com>
2026-01-02 11:05:36 +0100

    Dodanie struktury pamieci dzielonej dla stanu statku

--- include/common.h
+++ include/common.h
@@ -7,6 +7,9 @@
 #include <errno.h>
 #include <signal.h>
 #include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
 
 extern int N;
 extern int M;
@@ -15,11 +18,21 @@ extern int T1;
 extern int T2;
 extern int R;
 
+extern int shm_id;
+extern int sem_id;
+extern shared_state_t *state;
+
 typedef enum {
 LOADING,
 SAILING,
 UNLOADING,
 STOPPED
 } ship_state_t;
+
+typedef struct {
+int passengers_on_ship;
+int bikes_on_ship;
+int passengers_on_bridge;
+} shared_state_t;
 
 #endif
