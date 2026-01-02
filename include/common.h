e7bee7fcb7320c29eb2102fd75d5bf5168f9e664
Monika Toczek <moniat598@gmail.com>
2026-01-02 14:12:11 +0100

    DOdanie funkcji synchronizacji semaforem (lock/unlock)

--- include/common.h
+++ include/common.h
@@ -18,10 +18,6 @@ extern int T1;
 extern int T2;
 extern int R;
 
-extern int shm_id;
-extern int sem_id;
-extern shared_state_t *state;
-
 typedef enum {
 LOADING,
 SAILING,
@@ -34,5 +30,19 @@ int passengers_on_ship;
 int bikes_on_ship;
 int passengers_on_bridge;
 } shared_state_t;
+
+static inline void sem_lock(int sem_id) {
+    struct sembuf op = {0, -1, 0};
+    semop(sem_id, &op, 1);
+}
+
+static inline void sem_unlock(int sem_id) {
+    struct sembuf op = {0, 1, 0};
+    semop(sem_id, &op, 1);
+}
+
+extern int shm_id;
+extern int sem_id;
+extern shared_state_t *state;
 
 #endif
