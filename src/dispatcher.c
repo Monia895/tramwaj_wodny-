6ce378fd903158c3e2146b6cd8ec862fca433ef9
Monika Toczek <moniat598@gmail.com>
2026-01-04 17:28:44 +0100

    Dodano proces dyspozytora sterujacy przebiegiem symulacji z obsluga sygnalow

--- src/dispatcher.c
+++ src/dispatcher.c
@@ -1,16 +1,47 @@
 #include "common.h"
 #include "log.h"
-#include "ipc.h"
+#include <time.h>
+#include <signal.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) {
-   (void)argc;
-   (void)argv;
+int main(void) {
+   srand(getpid() ^ time(NULL));
 
-   ipc_init();
+   key_t key = ftok(".", 'S');
+   shm_id = shmget(key, sizeof(shared_state_t), 0);
+   sem_id = semget(key, SEM_COUNT, 0);
+   state = shmat(shm_id, NULL, 0);
 
-   log_event("DYSPOZYTOR: Gotowy do pracy");
+   log_event("DYSPOZYTOR: Uruchomiony");
 
-   printf("DYSPOZYTOR: Monitoruje rejsy\n");
+   while (1) {
+       sleep(rand() % 8 + 3);
+
+       sem_lock(sem_id, SEM_STATE);
+
+       if (state->ship_state == FINISHED) {
+           sem_unlock(sem_id, SEM_STATE);
+           break;
+       }
+
+       pid_t captain_pid = state->captain_pid;
+       ship_state_t st = state->ship_state;
+       sem_unlock(sem_id, SEM_STATE);
+
+       if (st == LOADING && (rand() % 2)) {
+           log_event("DYSPOZYTOR: Wysylam SIGUSR1");
+           kill(captain_pid, SIGUSR1);
+       }
+
+       if (rand() % 10 == 0) {
+           log_event("DYSPOZYTOR: Wysylam SIGUSR2");
+           kill(captain_pid, SIGUSR2);
+           break;
+       }
+   }
+
+   log_event("DYSPOZYTOR: Koncze prace");
+   shmdt(state);
    return 0;
 }
 
